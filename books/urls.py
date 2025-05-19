e01360a0568f53b81ecc79066c8fcc25435ef327
mihail <mihail.bashkatov@gmail.com>
2025-05-19 22:49:13 +0200

    Start CRUD for BookDetail in books/urls.py

--- books/urls.py
+++ books/urls.py
@@ -1,22 +1,23 @@
 from django.urls import path
 
 from books.apps import BooksConfig
+from books.views import BooksListAPIView, BookRetrieveAPIView, BookUpdateAPIView, BookDestroyAPIView
 
 app_name = BooksConfig.name
 
 
-# urlpatterns = [
-#     path("habit/create/", HabitCreateAPIView.as_view(), name="habit-create"),
-#     path("books/", HabitsListAPIView.as_view(), name="books-list"),
-#     path("user/books/", HabitsUserListAPIView.as_view(), name="books-user-list"),
-#
-#     path("habit/<int:pk>/", HabitRetreiveAPIView.as_view(), name="habit-detail"),
-#     path(
-#         "habit/update/<int:pk>/", HabitUpdateAPIView.as_view(), name="habit-update"
-#     ),
-#     path(
-#         "habit/delete/<int:pk>/", HabitDestroyAPIView.as_view(), name="habit-delete"
-#     ),
-#     path("habit/public/<int:pk>/", PublicAPIView.as_view(), name="public-habit"),
-#
-# ]
+urlpatterns = [
+    # path("habit/create/", HabitCreateAPIView.as_view(), name="habit-create"),
+    path("books/", BooksListAPIView.as_view(), name="books-list"),
+    # path("user/books/", HabitsUserListAPIView.as_view(), name="books-user-list"),
+    #
+    path("book/<int:pk>/", BookRetrieveAPIView.as_view(), name="book-detail"),
+    path(
+        "book/update/<int:pk>/", BookUpdateAPIView.as_view(), name="book-update"
+    ),
+    path(
+        "book/delete/<int:pk>/", BookDestroyAPIView.as_view(), name="book-delete"
+    ),
+    # path("habit/public/<int:pk>/", PublicAPIView.as_view(), name="public-habit"),
+
+]
