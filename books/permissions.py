18078e81c4c7b791fe005752096f6eca04d79e6b
mihail <mihail.bashkatov@gmail.com>
2025-05-19 22:50:35 +0200

    Adding IsLibrarian permission in books/permissions.py

--- books/permissions.py
+++ books/permissions.py
@@ -1,25 +1,36 @@
 from rest_framework import permissions
 
 
-class IsOwner(permissions.BasePermission):
+# class IsOwner(permissions.BasePermission):
+#     """
+#     Object-level permission to only allow user of an object(habit) to edit it.
+#     """
+#
+#     def has_object_permission(self, request, view, obj):
+#
+#         if obj.habit_user == request.user:
+#             return True
+#         return False
+#
+#
+# class IsOwnerNiceHabit(permissions.BasePermission):
+#     """
+#     Object-level permission to only allow user of an object(habit) to edit it.
+#     """
+#
+#     def has_object_permission(self, request, view, obj):
+#
+#         if obj.nice_habit_user == request.user:
+#             return True
+#         return False
+
+class IsLibrarian(permissions.BasePermission):
     """
-    Object-level permission to only allow user of an object(habit) to edit it.
+    Object-level permission to only allow librarian of an object(book) to edit it.
     """
 
     def has_object_permission(self, request, view, obj):
 
-        if obj.habit_user == request.user:
-            return True
-        return False
-
-
-class IsOwnerNiceHabit(permissions.BasePermission):
-    """
-    Object-level permission to only allow user of an object(habit) to edit it.
-    """
-
-    def has_object_permission(self, request, view, obj):
-
-        if obj.nice_habit_user == request.user:
+        if request.user.is_librarian:
             return True
         return False
