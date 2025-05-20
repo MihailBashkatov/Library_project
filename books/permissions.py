f5ae12a7f46788714153ff0d5d63cedf4e779c07
mihail <mihail.bashkatov@gmail.com>
2025-05-20 22:22:28 +0200

    Adding permission IsLibrarianAddBook in books/permissions.py

--- books/permissions.py
+++ books/permissions.py
@@ -1,16 +1,16 @@
 from rest_framework import permissions
 
 
-# class IsOwner(permissions.BasePermission):
-#     """
-#     Object-level permission to only allow user of an object(habit) to edit it.
-#     """
-#
-#     def has_object_permission(self, request, view, obj):
-#
-#         if obj.habit_user == request.user:
-#             return True
-#         return False
+class IsOwner(permissions.BasePermission):
+    """
+    Object-level permission to only allow user of an object(book) to view it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+
+        if obj.client == request.user:
+            return True
+        return False
 #
 #
 # class IsOwnerNiceHabit(permissions.BasePermission):
@@ -30,6 +30,17 @@ class IsLibrarian(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
+
+        if request.user.is_librarian:
+            return True
+        return False
+
+class IsLibrarianAddBook(permissions.BasePermission):
+    """
+    Request permission to only allow librarian to add book
+    """
+
+    def has_permission(self, request, view):
 
         if request.user.is_librarian:
             return True
