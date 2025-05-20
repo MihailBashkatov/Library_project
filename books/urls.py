f61f4de74dcd64824835e99745a3f2172306608c
mihail <mihail.bashkatov@gmail.com>
2025-05-20 22:19:09 +0200

    Continuing CRUD for books in books/urls.py

--- books/urls.py
+++ books/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 
 from books.apps import BooksConfig
-from books.views import BooksListAPIView, BookRetrieveAPIView, BookUpdateAPIView, BookDestroyAPIView
+from books.views import BooksListAPIView, BookRetrieveAPIView, BookUpdateAPIView, BookDestroyAPIView, \
+    BooksUserListAPIView, BookCreateAPIView
 
 app_name = BooksConfig.name
 
 
 urlpatterns = [
-    # path("habit/create/", HabitCreateAPIView.as_view(), name="habit-create"),
+    path("book/create/", BookCreateAPIView.as_view(), name="book-create"),
     path("books/", BooksListAPIView.as_view(), name="books-list"),
-    # path("user/books/", HabitsUserListAPIView.as_view(), name="books-user-list"),
-    #
+    path("user/books/", BooksUserListAPIView.as_view(), name="books-user-list"),
+
     path("book/<int:pk>/", BookRetrieveAPIView.as_view(), name="book-detail"),
     path(
         "book/update/<int:pk>/", BookUpdateAPIView.as_view(), name="book-update"
