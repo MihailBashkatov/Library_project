2d880508b3fde106e8e7b376a12a808cc2c32e08
mihail <mihail.bashkatov@gmail.com>
2025-05-22 22:35:35 +0200

    Creating IsBookTaken Validator in books/validators.py

--- books/validators.py
+++ books/validators.py
@@ -1,18 +1,50 @@
 from rest_framework.serializers import ValidationError
+from django.utils import timezone
+from django.utils.timezone import localtime
+from books.models import BookDetail, BookGeneral
+# from books.serializers import BookDetailSerializer
 
 
-class IsVolumeExist:
+class IsBookTaken:
     """
-    Adding validator to check if volume exists, then number of pages van be null"""
+    Adding validator to check if client who has a book exists,
+    then not possible to assign another client until client value  set for Null"""
+
+    requires_context = True
 
     def __init__(self, field):
         self.field = field
 
-    def __call__(self, value):
-        book_volume = value.get("book_volume")
-        if len(book_volume) == 0:
-            # if time_duration > 120:
-            raise ValidationError("Time duration shall not be more than 120 seconds")
+    def __call__(self, value, serializer):
+        print('VALID')
+        book_id = (str(serializer.context.get("request").path).split('update/')[-1].strip('/'))
+        client = value.get("client")
+        if BookDetail.objects.get(id=book_id).client:
+            if client:
+                current_client = BookDetail.objects.filter(client=client, id=book_id).exists()
+
+                if not current_client:
+                    raise ValidationError(f"book is already taken by client {client.email}. "
+                                          f"Books need to be return before assigning for a new client")
+        if client is not None:
+            print('time')
+            # book = BookDetailSerializer.objects.get(id=book_id)
+            print(serializer.instance.taken_by_client)
+            # book.taken_by_client = localtime(timezone.now()).strftime("%Y-%m-%d %H:%M")  # Get local current time
+            serializer.instance.taken_by_client = localtime(timezone.now()) # Get local current time
+            # print(serializer.taken_by_client)
+            # print(book.taken_by_client)
+        #     book.page_amount = 1
+            serializer.instance.save()
+        #     print(book.taken_by_client)
+        #     book.page_amount = 200000000
+        #     books = BookDetail.objects.all()
+        #     for book in books:
+        #         print(book.id, book.taken_by_client)
+        #
+        else:
+            print('null')
+        #
 
 
 # class HabitPeriodValidator:
