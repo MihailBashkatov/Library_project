5d36eb73ef4b36cdc06899380c8d334ee7b45247
mihail <mihail.bashkatov@gmail.com>
2025-05-24 19:28:03 +0200

    Finalizing IsBookTaken validator in books/validators.py

--- books/validators.py
+++ books/validators.py
@@ -3,7 +3,9 @@ from datetime import timedelta
 from rest_framework.serializers import ValidationError
 from django.utils import timezone
 from django.utils.timezone import localtime
-from books.models import BookDetail, BookGeneral
+from books.models import BookDetail, BookGeneral, Archive
+
+
 # from books.serializers import BookDetailSerializer
 
 
@@ -12,8 +14,10 @@ from books.models import BookDetail, BookGeneral
 
 class IsBookTaken:
     """
-    Adding validator to check if client who has a book exists,
-    then not possible to assign another client until client value  set for Null"""
+    Adding validator to check if client, who has a book ,exists,
+    then not possible to assign another client until client value set for Null
+    Also there is a logic to set new order in tha tbale Archive and steps in case client wants to prolong
+    ordered book"""
 
     requires_context = True
 
@@ -21,30 +25,85 @@ class IsBookTaken:
         self.field = field
 
     def __call__(self, value, serializer):
-        # book = serializer.instance.book_volume.all()[0].page_amount
-        # print(book)
+
+        # Get BookDetailClientSerializer
         book = serializer.instance
+
+        # Get BookDetail ID
         book_id = (str(serializer.context.get("request").path).split('update/')[-1].strip('/'))
+
+        # Get client
         client = value.get("client")
-        if BookDetail.objects.get(id=book_id).client:
+
+        # If client exists, get client's user_card
+        if client:
+            user_card = client.user_card
+
+        # Set local time
+        time_now = localtime(timezone.now())
+
+        # Get ArchiveOrderSerializer from instance
+        archived_order = serializer.instance.archive_order
+
+        # Logic if client is chosen as None. It is needed when client returns a book and then
+        # book is not linked to any client, taken time and due date are becoming Null
+        if not client:
+            book.taken_by_client = None  # Set null
+            book.due_date = None  # Set null
+            book.client = None # Set null
+            book.save()
+
+            # Logic to find if order for the book exists. If exists, then in Archive table this order sets time
+            # of the book return. In case order does not exist, nothing happen
+
+            search_order = archived_order.filter(title=book, return_date=None).exists()
+            if search_order:
+                order = archived_order.get(title=book, return_date=None)
+                order.return_date = time_now
+                order.save()
+            else:
+                pass
+
+        # Logic if any client is already having book (ordered it earlier). It is needed when client wants to
+        # prolong the order. If another client will try to book this book, it will not be allowed
+        elif BookDetail.objects.get(id=book_id).client:
             if client:
                 current_client = BookDetail.objects.filter(client=client, id=book_id).exists()
 
+                # Check if targeted client is a client, who already has a book
                 if not current_client:
                     raise ValidationError(f"book is already taken by client {client.email}. "
                                           f"Books need to be return before assigning for a new client")
-                book.taken_by_client = localtime(timezone.now()) # Get local current time
-                book.due_date = book.taken_by_client + timedelta(days=30) # Set time for returning the book
-                book.save()
 
-        if client is not None:
-            book.taken_by_client = localtime(timezone.now())  # Get local current time
+                # Search the book, which is on hand at current client
+                search_order = archived_order.filter(title=book, return_date=None).exists
+                if search_order:
+                    order = archived_order.get(title=book, return_date=None)
+
+                    # Checks how many times order was prolonged. Maximum is 2 times
+                    if order.order_continued_times == 2:
+                        raise ValidationError(f"Book order is already renewed 2 times. No possibility to renew more")
+                    order.order_continued_times += 1
+                    order.save()
+
+                    # Sets new time of order in the table BookDetail and new due date
+                    book.taken_by_client = time_now  # Get local current time
+                    book.due_date = book.taken_by_client + timedelta(days=30)  # Set time for returning the book
+                    book.save()
+
+
+        else:
+
+            # For new order, sets time in the table BookDetail
+            book.taken_by_client = time_now  # Get local current time
+
+            # Adding new order to table Archive
+            archived_order.create(title=book, user_card=user_card, taken_by_client=time_now)
+
+            # Sets due date time in the table BookDetail
             book.due_date = book.taken_by_client + timedelta(days=30)  # Set time for returning the book
             book.save()
-        else:
-            book.taken_by_client = None # Set null
-            book.due_date = None  # Set null
-            book.save()
+
 
 
 
