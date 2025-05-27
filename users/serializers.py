93245c3ef01063c6e05af9a15c7d512b12cb235c
mihail <mihail.bashkatov@gmail.com>
2025-05-27 11:32:43 +0200

    Adding logic to create user_card, during user registration in users/serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-
 from users.models import User
 
 
@@ -9,3 +8,29 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "email", "user_card", "telegram_chat_id"]
+
+    def create(self, validated_data):
+        """Create users_card for the user"""
+        user_card = []
+        last_user_card = str(User.objects.all().last().id + 1)
+        if len(last_user_card) == 1:
+            user_card = "".join(["0", "0", "0", last_user_card[0]])
+        elif len(last_user_card) == 2:
+            user_card = "".join(["0", "0", last_user_card[0], last_user_card[1]])
+        elif len(last_user_card) == 3:
+            user_card = "".join(
+                ["0", last_user_card[0], last_user_card[1], last_user_card[2]]
+            )
+        elif len(last_user_card) == 4:
+            user_card = "".join(
+                [
+                    last_user_card[0],
+                    last_user_card[1],
+                    last_user_card[2],
+                    last_user_card[3],
+                ]
+            )
+
+        validated_data["user_card"] = user_card
+
+        return User.objects.create(**validated_data)
