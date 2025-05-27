6bb98db4c4afd36a80d4d71dc4a466dbf59d0792
mihail <mihail.bashkatov@gmail.com>
2025-05-27 17:59:17 +0200

    Adding send_telegram_message in books/services.py

--- books/services.py
+++ books/services.py
@@ -3,11 +3,11 @@ import requests
 from config.settings import BOT_TOKEN, TELEGRAM_URL
 
 
-def send_telegram_message(telegram_chat_id, message):
+def send_telegram_message(client_tg_chat_id, message):
     """Function to send a message to user via Telegram"""
 
     params = {
         "text": message,
-        "chat_id": telegram_chat_id,
+        "chat_id": client_tg_chat_id,
     }
     requests.get(f"{TELEGRAM_URL}{BOT_TOKEN}/sendMessage", params=params)
