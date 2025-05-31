3f471a1461f3b401dacea49c71d03d2b65fb9272
mihail <mihail.bashkatov@gmail.com>
2025-05-31 19:05:58 +0200

    Modifying bokks/tests.py

--- books/tests.py
+++ books/tests.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from books.models import (Archive, Author, BookDetail, BookFeature,
+from books.models import (Author, BookDetail, BookFeature,
                           BookFinance, BookGeneral, BookGenre, Library)
 from users.models import User
 
