7e75dc8bc9d3cddd3c25b5db1f1d903ef803b126
mihail <mihail.bashkatov@gmail.com>
2025-06-01 12:17:59 +0200

    Modifying users/permissions.py

--- users/permissions.py
+++ users/permissions.py
@@ -1,8 +1,16 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
 
-class IsUser(BasePermission):
-    message = "No access"
+class IsUser(permissions.BasePermission):
+    """
+    Object-level permission to only allow user of an object(book) to view it.
+    """
 
     def has_object_permission(self, request, view, obj):
-        return obj == request.user
+        print(request.user)
+        print(view)
+        print(obj)
+
+        if obj == request.user:
+            return True
+        return False
